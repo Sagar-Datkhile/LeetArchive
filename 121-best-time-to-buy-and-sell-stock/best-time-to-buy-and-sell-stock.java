e211350d76c108fad9e2a568d0a4e28a0ef04bd4
Sagar Datkhile <92229065+Sagar-Datkhile@users.noreply.github.com>
2025-07-25 20:38:58 +0530

    Time: 2 ms (72.52%) | Memory: 61.7 MB (64.00%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minValue = prices[0];
+        int result = 0;
+    
+        for(int i=0; i<prices.length; i++){
+            minValue = Math.min(minValue,prices[i]);
+            result = Math.max(result, prices[i] - minValue);
+        }
+
+        return result;
+    }
+}
