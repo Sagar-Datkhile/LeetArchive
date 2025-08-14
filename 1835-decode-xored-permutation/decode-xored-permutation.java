54eeb47656a768b0f551e560e023205c5f53913a
Sagar Datkhile <92229065+Sagar-Datkhile@users.noreply.github.com>
2025-08-14 22:40:44 +0530

    Time: 2 ms (100.00%) | Memory: 64.7 MB (33.95%) - LeetSync

---
+++ 1835-decode-xored-permutation/decode-xored-permutation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] decode(int[] encoded) {
+        int n = encoded.length+1;
+        int total = 0;
+        int oddXor = 0;
+
+        int[] decoded = new int[n];
+        for(int i=1; i<=n; i++){
+            total ^= i;
+        }
+
+        for(int i=1; i<=n-2; i+=2){
+            oddXor = oddXor ^ encoded[i];
+        }
+
+        decoded[0] = total ^ oddXor;
+        for(int i=0; i<n-1; i++){
+            decoded[i+1] = decoded[i] ^ encoded[i];
+        }
+
+        return decoded;
+        
+    }
+}
