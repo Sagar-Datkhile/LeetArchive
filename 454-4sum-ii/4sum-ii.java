911fbe8d47df40866c5e5db52b1ee6fe8e477924
Sagar Datkhile <92229065+Sagar-Datkhile@users.noreply.github.com>
2025-10-06 00:10:49 +0530

    Time: 110 ms (65.84%) | Memory: 44.8 MB (92.35%) - LeetSync

--- 454-4sum-ii/4sum-ii.java
+++ 454-4sum-ii/4sum-ii.java
@@ -1,28 +1,21 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-           HashMap<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-
-        // Step 1: Compute all possible sums of nums1 and nums2
-        // and store their frequencies in the map
-        for (int a : nums1) {
-            for (int b : nums2) {
-                int sum = a + b;
-                map.put(sum, map.getOrDefault(sum, 0) + 1);
-            }
+    HashMap<Integer,Integer> map = new HashMap<>();
+    int count = 0;
+    for(int a:nums1){
+        for(int b:nums2){
+            int key = a+b;
+            map.put(key, map.getOrDefault(key,0) +1);
         }
+    }
 
-        // Step 2: Compute all possible sums of nums3 and nums4
-        // and check if the negated sum exists in the map
-        for (int c : nums3) {
-            for (int d : nums4) {
-                int sum = c + d;
-                if (map.containsKey(-sum)) {
-                    count += map.get(-sum);
-                }
-            }
+    for(int c:nums3){
+        for(int d: nums4){
+            int key = (-1)*(c+d);
+            count+= map.getOrDefault(key,0);
         }
+    }
+    return count;
 
-        return count;
     }
 }
