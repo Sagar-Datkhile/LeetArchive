d01eb028e4f5dfa4a7b8a26324c7e3c2ec640f03
Sagar Datkhile <92229065+Sagar-Datkhile@users.noreply.github.com>
2025-11-05 01:46:02 +0530

    Time: 79 ms (65.50%) | Memory: 71.6 MB (5.53%) - LeetSync

---
+++ 398-random-pick-index/random-pick-index.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    Map<Integer, List<Integer>> map;
+    Random rand;
+
+    public Solution(int[] nums) {
+        map = new HashMap<>();
+        rand = new Random();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+    }
+
+    public int pick(int target) {
+        List<Integer> list = map.get(target);
+        return list.get(rand.nextInt(list.size()));
+    }
+}
