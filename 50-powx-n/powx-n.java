18f7f03cf06aa005345b881f9656f29f9899ed30
Sagar Datkhile <92229065+Sagar-Datkhile@users.noreply.github.com>
2025-11-21 22:01:12 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (14.16%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        // return Math.pow(x,n);
+        double ans = 1.0;
+        long long_n = n; // Copy variable of power
+        if(long_n<0){
+            long_n = (-1)*long_n;
+        }
+        while(long_n>0){
+            if(long_n % 2 == 1){
+                ans = ans * x;
+                long_n -= 1;
+            }else{
+                x = x*x;
+                long_n /= 2;
+            }
+        }
+        if(n<0){
+            ans = (double)(1.0) / (double)(ans);
+        }
+        return ans;
         
     }
 }
