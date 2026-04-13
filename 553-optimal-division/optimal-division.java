f23951820a3251e15dc6850e4c12ff5947cf5eeb
Sagar Datkhile <92229065+Sagar-Datkhile@users.noreply.github.com>
2026-04-13 22:55:01 +0530

    Time: 3 ms (84.83%) | Memory: 42.9 MB (69.94%) - LeetSync

--- 553-optimal-division/optimal-division.java
+++ 553-optimal-division/optimal-division.java
@@ -2,17 +2,14 @@ class Solution {
     public String optimalDivision(int[] nums) {
         int n = nums.length;
         
-        // If only one number
         if (n == 1) {
             return String.valueOf(nums[0]);
         }
         
-        // If two numbers
         if (n == 2) {
             return nums[0] + "/" + nums[1];
         }
         
-        // For more than 2 numbers
         StringBuilder result = new StringBuilder();
         result.append(nums[0]).append("/(").append(nums[1]);
         
