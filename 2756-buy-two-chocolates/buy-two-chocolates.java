9ce1df2f5d1dd303f1e27383527969b877a00caa
Sagar Datkhile <92229065+Sagar-Datkhile@users.noreply.github.com>
2026-08-28 00:45:47 +0530

    Time: 3 ms (46.73%) | Memory: 46.3 MB (78.69%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.java
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -1,13 +1,14 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
         Arrays.sort(prices);
-        for(int i=0; i<prices.length-1; i++){
+        for(int i=0; i<prices.length -1; i++){
             int sum = prices[i] + prices[i+1];
             if((money-sum) < 0){
                 continue;
             }else{
                 return money-sum;
             }
+
         }
         return money;
     }
